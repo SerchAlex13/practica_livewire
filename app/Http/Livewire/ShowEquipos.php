fa38a3f259cba99b473efc05ac85282038ef3fc0
SerchAlex13 <sergio.rosales5262@alumnos.udg.mx>
2023-07-04 16:29:52 -0600

    Crear proyecto

---
+++ app/Http/Livewire/ShowEquipos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ShowEquipos extends Component
+{
+    public function render()
+    {
+        return view('livewire.show-equipos');
+    }
+}
